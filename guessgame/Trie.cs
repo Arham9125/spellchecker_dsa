37d5344e1d6572b01bafb41121d18f13c7ffe271
Arham Khan <arham9125@gmail.com>
2023-07-23 18:08:39 +0500

    spellchecker

---
+++ guessgame/Trie.cs
@@ -0,0 +1,65 @@
+﻿public class Trie
+{
+    private TrieNode root;
+
+    public Trie()
+    {
+        root = new TrieNode();
+    }
+
+    public void InsertWord(string word)
+    {
+        TrieNode currNode = root;
+        foreach (char c in word)
+        {
+            int index = c - 'a';
+            if (currNode.Children[index] == null)
+            {
+                currNode.Children[index] = new TrieNode();
+            }
+            currNode = currNode.Children[index];
+        }
+        currNode.Terminating = true;
+    }
+
+    public bool SearchWord(string word)
+    {
+        TrieNode currNode = root;
+        foreach (char c in word)
+        {
+            int index = c - 'a';
+            if (currNode.Children[index] == null)
+            {
+                return false;
+            }
+            currNode = currNode.Children[index];
+        }
+        return currNode.Terminating;
+    }
+
+    public bool DeleteWord(string word)
+    {
+        TrieNode currNode = root;
+        foreach (char c in word)
+        {
+            int index = c - 'a';
+            if (currNode.Children[index] == null)
+            {
+                return false;
+            }
+            currNode = currNode.Children[index];
+        }
+        currNode.Terminating = false;
+        return true;
+    }
+
+    public bool UpdateWord(string oldWord, string newWord)
+    {
+        bool ret = DeleteWord(oldWord);
+        if (ret)
+        {
+            InsertWord(newWord);
+        }
+        return ret;
+    }
+}
---
+++ guessgame/TrieNode.cs
@@ -0,0 +1,11 @@
+﻿public class TrieNode
+{
+    public bool Terminating { get; set; }
+    public TrieNode[] Children { get; set; }
+
+    public TrieNode()
+    {
+        Terminating = false;
+        Children = new TrieNode[26];
+    }
+}
